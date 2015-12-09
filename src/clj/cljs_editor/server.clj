25e2640699e8d767cf8ca31411ba1c6d65bd2497
OHTA Shogo <athos0220@gmail.com>
2015-12-09 21:14:20 +0900

    Implement sever side APIs

--- src/clj/cljs_editor/server.clj
+++ src/clj/cljs_editor/server.clj
@@ -1,28 +1,55 @@
 (ns cljs-editor.server
   (:require [clojure.java.io :as io]
             [cljs-editor.dev :refer [is-dev? inject-devmode-html browser-repl start-figwheel]]
-            [compojure.core :refer [GET defroutes]]
+            [environ.core :refer [env]]
+            [compojure.core :refer [GET POST defroutes routes context]]
             [compojure.route :refer [resources]]
             [net.cgrand.enlive-html :refer [deftemplate]]
             [net.cgrand.reload :refer [auto-reload]]
             [ring.middleware.reload :as reload]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [environ.core :refer [env]]
+            [ring.util.response :as res]
+            [ring.middleware.format :refer [wrap-restful-format]]
             [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
 (deftemplate page (io/resource "index.html") []
   [:body] (if is-dev? inject-devmode-html identity))
 
-(defroutes routes
+(def file-contents (atom {"foo" "foo\nfoo\nfoo"
+                          "bar" "bar\nbar\nbar"
+                          "baz" "baz\nbaz\nbaz"}))
+
+(defn response [status]
+  (fn f
+    ([] (f {}))
+    ([data]
+     (res/response (merge {:status status} data)))))
+
+(def ok (response :ok))
+(def fail (response :failure))
+
+(def api-routes
+  (-> (routes
+       (GET "/filenames" []
+            (ok {:names (vec (keys @file-contents))}))
+       (GET "/files/:filename" [filename]
+            (ok {:content (get @file-contents filename)}))
+       (POST "/files/:filename" [filename content]
+             (swap! file-contents assoc filename content)
+             (ok)))
+      (wrap-restful-format :formats [:edn])))
+
+(defroutes app-routes
+  (context "/api" [] #'api-routes)
+  (GET "/" [] (page))
   (resources "/")
-  (resources "/react" {:root "react"})
-  (GET "/*" req (page)))
+  (resources "/react" {:root "react"}))
 
 (def http-handler
   (if is-dev?
-    (reload/wrap-reload (wrap-defaults #'routes api-defaults))
-    (wrap-defaults routes api-defaults)))
+    (reload/wrap-reload (wrap-defaults #'app-routes api-defaults))
+    (wrap-defaults app-routes api-defaults)))
 
 (defn run-web-server [& [port]]
   (let [port (Integer. (or port (env :port) 10555))]
